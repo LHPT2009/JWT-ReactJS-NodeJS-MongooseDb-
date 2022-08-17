089aa62e8860bd9b02211f362ccdc60bf462d72f
lehuynhphuongtung <lehuynhphuongtung02@gmail.com>
2022-08-17 22:40:49 +0700

    loginandregister

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -2,6 +2,8 @@ const authController = require("../controllers/authControllers");
 
 const router = require("express").Router();
 
-router.post("/register",authController.registerUser);
+router.post("/register", authController.registerUser);
+
+router.post("/login", authController.loginUser);
 
 module.exports = router;
